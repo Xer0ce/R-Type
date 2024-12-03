532e2047a04064a4c3bacf7ee51ee0c94983e3bc
Louis Huguet <louis.huguet@epitech.eu>
2024-12-03 13:16:11 +0100

    UPDATE : include IProtocol

--- src/server/IProtocol.hpp
+++ src/server/IProtocol.hpp
@@ -7,6 +7,8 @@
 
 #pragma once
 
+#include <iostream>
+
 class IProtocol {
     public:
         IProtocol();
--- src/server/udp/UDP.hpp
+++ src/server/udp/UDP.hpp
@@ -7,7 +7,6 @@
 
 #pragma once
 
-#include <iostream>
 #include "AProtocol.hpp"
 
 class UDP : public AProtocol {
