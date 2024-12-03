4b0ab5673122aa0d4bafe5214afb998e05f9c849
albe2 <alberic.flour-de-saegher@epitech.eu>
2024-12-03 15:09:58 +0100

    ADD: 3 first compoenent

---
+++ src/graphical/Components/Velocity.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** RType
+** File description:
+** Velocity
+*/
+
+#ifndef VELOCITY_HPP
+#define VELOCITY_HPP
+
+struct Velocity {
+    float x, y;
+    Velocity(float x = 0, float y = 0) : x(x), y(y) {}
+};
+
+#endif // VELOCITY_HPP
