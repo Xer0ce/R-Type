189cfbef7f6edad7f98e19be039b6068e42fd37d
Felix Schrynemaekers <felix.schrynemaekers@epitech.eu>
2024-12-24 17:26:05 +0100

    ADD : entity type Enemy

--- src/ecs/EntitiesGestion.cpp
+++ src/ecs/EntitiesGestion.cpp
@@ -8,15 +8,24 @@ Entities create_player_entity(Registry &r, Position position, Velocity velocity,
   r.add_component<Health>(entity, std::move(health));
   // r.add_component<Draw>(entity, std::move(draw));
   r.add_component<Control>(entity, Control());
+  r.add_component<EntityType>(entity, EntityType::Player);
   return entity;
 }
 
-Entities create_enemy_entity(Registry &r) {
+Entities create_enemy_entity(Registry &r, Position position, Velocity velocity,
+                              Health health, Draw draw) {
   auto entity = r.spawn_entity();
+  r.add_component<Position>(entity, std::move(position));
+  r.add_component<Velocity>(entity, std::move(velocity));
+  r.add_component<Health>(entity, std::move(health));
+  // r.add_component<Draw>(entity, std::move(draw));
+  r.add_component<Control>(entity, Control());
+  r.add_component<EntityType>(entity, EntityType::Enemy);
   return entity;
 }
 
 Entities create_projectile_entity(Registry &r) {
   auto entity = r.spawn_entity();
+  r.add_component<EntityType>(entity, EntityType::Projectile);
   return entity;
 }
--- src/ecs/EntitiesGestion.hpp
+++ src/ecs/EntitiesGestion.hpp
@@ -1,38 +1,39 @@
-#ifndef ENTITIESGESTION_HPP
-#define ENTITIESGESTION_HPP
+  #ifndef ENTITIESGESTION_HPP
+  #define ENTITIESGESTION_HPP
 
-#include "../graphical/Components/Control.hpp"
-#include "../graphical/Components/Draw.hpp"
-#include "../graphical/Components/Health.hpp"
-#include "../graphical/Components/Position.hpp"
-#include "../graphical/Components/Velocity.hpp"
-#include "Entities.hpp"
-#include "Registry.hpp"
+  #include "../graphical/Components/Control.hpp"
+  #include "../graphical/Components/Draw.hpp"
+  #include "../graphical/Components/Health.hpp"
+  #include "../graphical/Components/Position.hpp"
+  #include "../graphical/Components/Velocity.hpp"
+  #include "Entities.hpp"
+  #include "Registry.hpp"
 
-enum class EntityType {
-  Player,
-  Enemy,
-  Projectile,
-};
+  enum class EntityType {
+    Player,
+    Enemy,
+    Projectile,
+  };
 
-Entities create_player_entity(Registry &r, Position position, Velocity velocity,
+  Entities create_player_entity(Registry &r, Position position, Velocity velocity,
+                                Health health, Draw draw);
+
+  Entities create_enemy_entity(Registry &r, Position position, Velocity velocity,
                               Health health, Draw draw);
 
-Entities create_enemy_entity(Registry &r);
+  Entities create_projectile_entity(Registry &r);
 
-Entities create_projectile_entity(Registry &r);
+  template <EntityType T, typename... Args>
+  Entities create_entity(Registry &r, Args &&...args) {
+    if constexpr (T == EntityType::Player) {
+      return create_player_entity(r, std::forward<Args>(args)...);
+    }
+    if constexpr (T == EntityType::Enemy) {
+      return create_enemy_entity(r, std::forward<Args>(args)...);
+    }
+    if constexpr (T == EntityType::Projectile) {
+      return create_projectile_entity(r);
+    }
+  }
 
-template <EntityType T, typename... Args>
-Entities create_entity(Registry &r, Args &&...args) {
-  if constexpr (T == EntityType::Player) {
-    return create_player_entity(r, std::forward<Args>(args)...);
-  }
-  if constexpr (T == EntityType::Enemy) {
-    return create_enemy_entity(r);
-  }
-  if constexpr (T == EntityType::Projectile) {
-    return create_projectile_entity(r);
-  }
-}
-
-#endif // ENTITIESGESTION_HPP
+  #endif // ENTITIESGESTION_HPP
