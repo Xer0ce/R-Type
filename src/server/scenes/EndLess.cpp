dae2e2f84c394111f322dafb4b1149efc32b7f9b
albe2 <alberic.flour-de-saegher@epitech.eu>
2025-01-14 11:30:57 +0100

    ADD: wave logic first gestion

--- src/server/scenes/EndLess.cpp
+++ src/server/scenes/EndLess.cpp
@@ -7,6 +7,34 @@
 
 #include "EndLess.hpp"
 
+static const std::string classicPath = "../src/game/config/endless/classic/";
+static const std::string miniBossPath = "../src/game/config/endless/mini_boss/";
+static const std::string bossPath = "../src/game/config/endless/boss/";
+
+static const std::string classicWave[] = {
+  classicPath + "classic_wave_1.json",
+  classicPath + "classic_wave_2.json",
+  classicPath + "classic_wave_3.json",
+  classicPath + "classic_wave_4.json",
+  classicPath + "classic_wave_5.json",
+  classicPath + "classic_wave_6.json",
+  classicPath + "classic_wave_7.json",
+  classicPath + "classic_wave_8.json",
+  classicPath + "classic_wave_9.json",
+  classicPath + "classic_wave_10.json"
+};
+
+static const std::string miniBossWave[] = {
+  miniBossPath + "mini_boss_wave_1.json",
+  miniBossPath + "mini_boss_wave_2.json",
+  miniBossPath + "mini_boss_wave_3.json",
+};
+
+static const std::string bossWave[] = {
+  bossPath + "boss_wave_1.json",
+  bossPath + "boss_wave_2.json",
+};
+
 EndLess::EndLess() {
   _name = "EndLess";
   _startCooldown = true;
@@ -17,6 +45,29 @@ EndLess::EndLess() {
 EndLess::~EndLess() {}
 
 void EndLess::init() { _wave = Wave(_ecs); }
+
+bool EndLess::waveIsClear() {
+  auto &entityType = _ecs->get_components<EntityType>();
+
+  for (std::size_t i = 0; i < entityType.size(); i++) {
+    if (entityType[i] == EntityType::Enemy) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void EndLess::waveGestion() {
+  static std::random_device rd;
+  static std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(0, 9);
+  int random = dis(gen);
+
+  if (waveIsClear()) {
+    std::cout << "load " << random << " wave" << std::endl;
+    _wave.load(classicWave[random], *_queue);
+  }
+}
 
 sceneType
 EndLess::loop(std::chrono::time_point<std::chrono::steady_clock> deltaTime) {
@@ -44,10 +95,10 @@ EndLess::loop(std::chrono::time_point<std::chrono::steady_clock> deltaTime) {
   if (!_startCooldown) {
     if (_firstRound) {
       _firstRound = false;
-      std::string path = "../src/game/config/endless/wave_type_1.json";
-      _wave.load(path, *_queue);
+      _wave.load(classicWave[0], *_queue);
     }
     if (now > deltaTime) {
+      waveGestion();
       enemy_system(_ecs);
       position_system_net(1, _ecs, _queue, _nextCorrectPosition);
       collision_system(_ecs, _queue);
--- src/server/scenes/EndLess.hpp
+++ src/server/scenes/EndLess.hpp
@@ -19,6 +19,8 @@ public:
   sceneType
   loop(std::chrono::time_point<std::chrono::steady_clock> deltaTime) override;
 
+  bool waveIsClear();
+  void waveGestion();
   void init() override;
 
 private:
