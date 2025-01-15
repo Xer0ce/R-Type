17772c33530c7664a4fb4f2b1177f4134ca3ed24
albe2 <alberic.flour-de-saegher@epitech.eu>
2025-01-15 09:59:06 +0100

    ADD: movment ia

--- src/game/systems/enemy/Balzy.cpp
+++ src/game/systems/enemy/Balzy.cpp
@@ -11,13 +11,21 @@ void balzy_ai(Registry *ecs) {
   auto &velocity = ecs->get_components<Velocity>();
   auto &position = ecs->get_components<Position>();
   auto &entityType = ecs->get_components<EntityType>();
+  auto &aiType = ecs->get_components<AiType>();
 
   for (std::size_t i = 0; i < entityType.size(); i++) {
     if (entityType[i] == EntityType::Enemy) {
-      if (position[i]->y <= 0) {
-        velocity[i]->y = 10;
-      } else if (position[i]->y >= 500) {
-        velocity[i]->y = -10;
+      if (aiType[i] == AiType::Passive) {
+        if (position[i]->y <= 0) {
+          velocity[i]->y = 6.5;
+        } else if (position[i]->y >= 800) {
+          velocity[i]->y = -6.5;
+        }
+        if (position[i]->x <= 400) {
+          velocity[i]->x = 6.5;
+        } else if (position[i]->x >= 1200) {
+          velocity[i]->x = -6.5;
+        }
       }
     }
   }
