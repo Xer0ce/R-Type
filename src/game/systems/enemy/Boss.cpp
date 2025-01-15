e2d1324257f8a3c5912f73b87d3e721615bbb25a
albe2 <alberic.flour-de-saegher@epitech.eu>
2025-01-15 13:40:15 +0100

    FIX: boss ai and passive ai

--- src/game/systems/enemy/Boss.cpp
+++ src/game/systems/enemy/Boss.cpp
@@ -11,36 +11,10 @@
 void boss_ai(Registry *ecs, std::size_t enemy) {
   auto &velocity = ecs->get_components<Velocity>();
   auto &position = ecs->get_components<Position>();
-  static auto lastChangeTime = std::chrono::steady_clock::now();
-  static bool isWaiting = false;
-  static bool direction = true;
 
-  auto now = std::chrono::steady_clock::now();
-  auto duration =
-      std::chrono::duration_cast<std::chrono::seconds>(now - lastChangeTime)
-          .count();
-
-  if (isWaiting) {
-    if (duration >= 2) {
-      isWaiting = false;
-      lastChangeTime = now;
-    } else {
-      velocity[enemy]->y = 0;
-      return;
-    }
-  }
   if (position[enemy]->y <= 0) {
-    velocity[enemy]->y = 0;
-    isWaiting = true;
-    direction = true;
-    lastChangeTime = now;
-  } else if (position[enemy]->y >= 395 && position[enemy]->y <= 405) {
-    velocity[enemy]->y = 0;
-    isWaiting = true;
-    direction = !direction;
-    lastChangeTime = now;
-  } else if (position[enemy]->y >= 750) {
-    velocity[enemy]->y = 0;
-    isWaiting = true;
+    velocity[enemy]->y = 3;
+  } else if (position[enemy]->y >= 500) {
+    velocity[enemy]->y = -3;
   }
 }
--- src/game/systems/enemy/Passive.cpp
+++ src/game/systems/enemy/Passive.cpp
@@ -11,9 +11,9 @@ void passive_ai(Registry *ecs, std::size_t enemy) {
   auto &velocity = ecs->get_components<Velocity>();
   auto &position = ecs->get_components<Position>();
 
-  if (position[enemy]->y <= 0) {
-    velocity[enemy]->y = 10;
-  } else if (position[enemy]->y >= 500) {
-    velocity[enemy]->y = -10;
+  if (position[enemy]->x <= 800) {
+    velocity[enemy]->x = 0;
+  } else if (position[enemy]->x > 800) {
+    velocity[enemy]->x = -10;
   }
 }
